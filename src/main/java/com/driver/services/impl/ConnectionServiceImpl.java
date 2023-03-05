e1eeadfeb1cdcdbee9667f684c78bff66d74133b
pappukr3631 <100488442+pappukr3631@users.noreply.github.com>
2023-03-05 17:24:19 +0530

    tt

--- src/main/java/com/driver/services/impl/ConnectionServiceImpl.java
+++ src/main/java/com/driver/services/impl/ConnectionServiceImpl.java
@@ -34,7 +34,7 @@ public class ConnectionServiceImpl implements ConnectionService {
         //      return the updated user. If multiple service providers allow you to connect to the country, use the
         //      service provider having smallest id.
 
-        countryName = countryName.toUpperCase();
+        /*countryName = countryName.toUpperCase();
         User user = userRepository2.findById(userId).get();
         if(user.getConnected()) {
             throw new Exception("Already connected");
@@ -84,6 +84,58 @@ public class ConnectionServiceImpl implements ConnectionService {
         userRepository2.save(user);
         serviceProviderRepository2.save(serviceProvider);
 
+        return user;*/
+        User user = userRepository2.findById(userId).get();
+        if(user.getMaskedIp()!=null){
+            throw new Exception("Already connected");
+        }
+        else if(countryName.equalsIgnoreCase(user.getOriginalCountry().getCountryName().toString())){
+            return user;
+        }
+        else {
+            if (user.getServiceProviderList()==null){
+                throw new Exception("Unable to connect");
+            }
+
+            List<ServiceProvider> serviceProviderList = user.getServiceProviderList();
+            int a = Integer.MAX_VALUE;
+            ServiceProvider serviceProvider = null;
+            Country country =null;
+
+            for(ServiceProvider serviceProvider1:serviceProviderList){
+
+                List<Country> countryList = serviceProvider1.getCountryList();
+
+                for (Country country1: countryList){
+
+                    if(countryName.equalsIgnoreCase(country1.getCountryName().toString()) && a > serviceProvider1.getId() ){
+                        a=serviceProvider1.getId();
+                        serviceProvider=serviceProvider1;
+                        country=country1;
+                    }
+                }
+            }
+            if (serviceProvider!=null){
+                Connection connection = new Connection();
+                connection.setUser(user);
+                connection.setServiceProvider(serviceProvider);
+
+                String cc = country.getCode();
+                int givenId = serviceProvider.getId();
+                String mask = cc+"."+givenId+"."+userId;
+
+                user.setMaskedIp(mask);
+                user.setConnected(true);
+                user.getConnectionList().add(connection);
+
+                serviceProvider.getConnectionList().add(connection);
+
+                userRepository2.save(user);
+                serviceProviderRepository2.save(serviceProvider);
+
+
+            }
+        }
         return user;
     }
     @Override
